45139f49b9cc98d662295b7a75e69bb1ea22c283
NGamerHere <codingdatta@gmail.com>
2025-03-17 22:27:50 +0530

    Initial commit

---
+++ src/main/java/com/coderstack/gymmatrix/GymmatrixApplication.java
@@ -0,0 +1,13 @@
+package com.coderstack.gymmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymmatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymmatrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coderstack/gymmatrix/GymmatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderstack.gymmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymmatrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
