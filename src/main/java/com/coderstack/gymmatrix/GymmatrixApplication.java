ea3b0570768f14a0d5e9e081df781ada16586fb4
N Datta sai ram <codingdatta@gmail.com>
2025-04-24 17:16:15 +0530

    removed some static methods

--- src/main/java/com/coderstack/gymmatrix/GymmatrixApplication.java
+++ src/main/java/com/coderstack/gymmatrix/GymmatrixApplication.java
@@ -1,17 +1,10 @@
 package com.coderstack.gymmatrix;
 
-import io.jsonwebtoken.Claims;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GymmatrixApplication {
-
-    public static int getUserID(HttpServletRequest request){
-        Claims claims = (Claims) request.getAttribute("sessionData");
-        return (Integer) claims.get("id");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GymmatrixApplication.class, args);
