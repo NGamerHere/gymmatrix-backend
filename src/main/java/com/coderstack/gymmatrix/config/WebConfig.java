504eae021fca4fa942ee8f407d99a9713d3f5678
N Datta sai ram <codingdatta@gmail.com>
2025-05-28 11:04:14 +0530

    added the WebConfig

--- src/main/java/com/coderstack/gymmatrix/config/WebConfig.java
+++ src/main/java/com/coderstack/gymmatrix/config/WebConfig.java
@@ -1,25 +1,38 @@
 package com.coderstack.gymmatrix.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class WebConfig {
+
+    @Value("${app.cors.allowed-origins}")
+    private String originLinks;
+
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        String[] allowedOrigins = originLinks.split("\\s*,\\s*");
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                logger.info("Allowed Origins");
+                for (String origin : allowedOrigins) {
+                    logger.info(origin);
+                }
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
 }
-
-
